804878e4cfdee96b19d36acb056cc20aee00a950
harshEPI <54037338+harshpol@users.noreply.github.com>
2022-12-27 23:23:14 +0530

    day 5

---
+++ Day4/main.py
@@ -0,0 +1,2 @@
+print("I am software developer ")
+print("I dont'n know who you are",10,"\n","I love you\n"*10)
---
+++ Day5/main.py
@@ -0,0 +1,30 @@
+# Example 1
+#This is a 'Single-Line Comment'
+print("This is a print statement.")
+
+#example2
+print("Hello World !!!") #Printing Hello World
+
+# Example 1: The use of ‘#’.
+
+#It will execute a block of code if a specified condition is true.
+#If the condition is false then it will execute another block of code.
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
+
+
+"""This is an if-else statement.
+It will execute a block of code if a specified condition is true.
+If the condition is false then it will execute another block of code."""
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
+
+
+# print("This doesnt "execute")
+print("This will \" execute")
